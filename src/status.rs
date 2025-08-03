d4dc41328c07afecf953aebe6c955975b6df0fa3
Guillaume Clement <gclement@baobob.org>
2025-08-03 17:20:47 +0200

    Write null for non existent fields, necessary for Home Assistant
    Signed-off-by: Guillaume Clement <gclement@baobob.org>

--- src/status.rs
+++ src/status.rs
@@ -3,13 +3,9 @@ use serde::Serialize;
 #[derive(Serialize, Debug)]
 pub struct StatusMessage {
     pub cpu_usage: f32,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub cpu_temp: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub disk_usage: Option<f32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub net_tx: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub net_rx: Option<f64>,
 }
 
